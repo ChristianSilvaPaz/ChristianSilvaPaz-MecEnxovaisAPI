483780429e2b52266a50b740f3db2dcc1307be0c
ChristianSilvaPaz <christiangunner@hotmail.com>
2023-05-11 17:03:05 -0300

    Add migration

---
+++ MecEnxovais.Infrastructure/Migrations/20230511195508_ProductImageDeletion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MecEnxovais.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductImageDeletion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Image",
+                table: "Products");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte>(
+                name: "Image",
+                table: "Products",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
